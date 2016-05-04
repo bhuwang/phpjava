68256e452648dac0632f968a92640ae3be1d02d0
bhuwang <bhuwangautam@lftechnology.com>
2016-05-04 14:30:31 +0545

    #999 - my first class.

---
+++ bhuwan/src/main/java/com/lftechnology/phpjava/MyFirstClassDemo.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.lftechnology.phpjava;
+
+/**
+ * @author bhuwan
+ *
+ */
+public class MyFirstClassDemo {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("This is my first class.");
+    }
+
+}
