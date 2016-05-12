b4f966303aa7ad7e423fb72b5a04edb33a2cfe47
Achyut Dahal <achyutdahal@lftechnology.com>
2016-05-12 18:37:25 +0545

    #2 Salutation Assignment committed

---
+++ achyut/src/main/java/com/lftechnology/one/Salutation.java
@@ -0,0 +1,15 @@
+package com.lftechnology.one;
+
+public class Salutation {
+	
+private String name;
+	
+	public Salutation(){
+		this.name = "Achyut";
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+
+}
